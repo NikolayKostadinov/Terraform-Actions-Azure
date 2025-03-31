89eaa38118f6a4520ef93656a6b5d6c057b88f8e
Nikolay.Kostadinov <Kostadinov.Nikolay.d@gmail.com>
2025-03-31 10:38:36 +0300

    add .gitignor

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.alwa.default_hostname                                          # The actual value to be outputted
-  description = "The public URL of the Application" # Description of what this output represents
-}
-
-output "webapp_ips" {
-  value = azurerm_linux_web_app.alwa.outbound_ip_addresses                                         # The actual value to be outputted
-  description = "The public IP address of the Application" # Description of what this output represents
-}
