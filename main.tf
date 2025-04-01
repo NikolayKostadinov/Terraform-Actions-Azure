b04abaa732fee0b5c8d548e2ee8227b81a6f001b
Nikolay.Kostadinov <Kostadinov.Nikolay.d@gmail.com>
2025-04-01 09:42:09 +0300

    storage_account_name

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "StorageRG"
-    storage_account_name = "taskboardstorage"
+    storage_account_name = "taskboardstoragenk"
     container_name       = "taskboardcontainer"
     key                  = "terraform.tfstate"
   }
